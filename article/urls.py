48ff909e73eaae9a49d51601fd6082cf4ee6b6ca
KimEunSu <misili17114@gmail.com>
2023-06-20 10:28:12 +0900

    fix: git merge

--- article/urls.py
+++ article/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleView.as_view(), name='article_view'),
-    path('<int:user_id>/', views.ArticleDetailView.as_view(), name='article_detail_view'),
-    path('bookmarks/', views.BookmarkCreate.as_view(), name='bookmark_create'),
-    path('bookmarks/<int:pk>/', views.BookmarkDelete.as_view(), name='bookmark_destroy'),
     path('<int:article_id>/', views.ArticleDetailView.as_view(), name='article_detail_view'),
     path('<int:article_id>/good/',views.ArticleGoodView.as_view(), name='article_good_view'),
     path('<int:article_id>/commentcr/', views.CommentView.as_view(), name='CommentforTest'),
